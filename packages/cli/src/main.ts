f34fc59ffa861e99f5246b6c36521db3179e9281
hoonveloper <jh6car@gmail.com>
2023-03-19 22:51:16 +0900

    feat(cli): create config.js

--- packages/cli/src/main.ts
+++ packages/cli/src/main.ts
@@ -1,19 +1,28 @@
 #!/usr/bin/env node
-import "source-map-support/register";
+import 'source-map-support/register'
 
-import packagesJson from "../package.json" assert { type: "json" };
-import { log } from "./utils/console";
-import chalk from "chalk";
+import packagesJson from '../package.json' assert { type: 'json' }
+import { log } from './utils/console'
+import chalk from 'chalk'
+import fs from 'fs'
 
-const argv = process.argv.slice(2);
-const prefixArgv = argv[0];
+const argv = process.argv.slice(2)
+const prefixArgv = argv[0]
 
 switch (prefixArgv) {
-  case "version": {
-    log("assetbox version", chalk.green(packagesJson.version));
-    break;
-  }
-  case "init": {
-    break;
-  }
+    case 'version': {
+        log('assetbox version', chalk.green(packagesJson.version))
+        break
+    }
+    case 'init': {
+        fs.writeFile(
+            '../config.js',
+            '//please input your own information.',
+            (err) => {
+                if (err) console.log('Error: ', err)
+                else console.log('config.js file is created.')
+            }
+        )
+        break
+    }
 }

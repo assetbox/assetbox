55094803967513fd314740133a9f87e385941348
gron1gh1 <gron1gh1@gmail.com>
2023-03-23 20:37:37 +0900

    feat(turbo): setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,6 +4,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
+  ignorePatterns: ["dist/**", "bin/**"],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",

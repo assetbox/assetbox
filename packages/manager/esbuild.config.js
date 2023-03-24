c25f099887fe980ca5e056b59f2771b23792c738
gron1gh1 <gron1gh1@gmail.com>
2023-03-24 16:19:14 +0900

    feat(manager): support esm, cjs build

---
+++ packages/manager/esbuild.config.js
@@ -0,0 +1,31 @@
+import { build } from 'esbuild'
+import packagesJson from './package.json' assert { type: 'json' }
+
+Promise.all([
+    build({
+        entryPoints: ['./src/entry.ts'],
+        bundle: true,
+        minify: true,
+        sourcemap: true,
+        format: 'cjs',
+        external: Object.keys({
+            ...packagesJson.dependencies,
+            ...packagesJson.devDependencies,
+        }),
+        platform: 'node',
+        outfile: './dist/entry.cjs',
+    }),
+    build({
+        entryPoints: ['./src/entry.ts'],
+        bundle: true,
+        minify: true,
+        sourcemap: true,
+        format: 'esm',
+        external: Object.keys({
+            ...packagesJson.dependencies,
+            ...packagesJson.devDependencies,
+        }),
+        platform: 'browser',
+        outfile: './dist/entry.mjs',
+    }),
+])

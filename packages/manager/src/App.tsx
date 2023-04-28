adcc603f2f3bb2905e29dc2afe8add15567d5a88
moonchop <minssu913@naver.com>
2023-04-28 20:41:43 +0900

    feat(manager): using trpc

--- packages/manager/src/App.tsx
+++ packages/manager/src/App.tsx
@@ -1,12 +1,22 @@
 import { useState } from "react";
 
+import { client } from "./client";
+
 export function App() {
   const [count, setCount] = useState(0);
-
+  const [text, setText] = useState("");
+  const postInput = async () => {
+    const res = await client.deleteAsset.mutate(text);
+    console.log(res);
+  };
   return (
     <div>
       <p>Count {count}</p>
       <button onClick={() => setCount(count + 1)}>+</button>
+      <div>
+        <input onChange={(e) => setText(e.target.value)} />
+        <button onClick={postInput}>버튼</button>
+      </div>
     </div>
   );
 }

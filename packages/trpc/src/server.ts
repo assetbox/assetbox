a459f47c9e3d7937f0d0fdcf772c7fdf39c04cda
moonchop <minssu913@naver.com>
2023-04-30 00:39:44 +0900

    feat(trpc): add renameAsset api

--- packages/trpc/src/server.ts
+++ packages/trpc/src/server.ts
@@ -1,25 +1,30 @@
 import { initTRPC } from "@trpc/server";
-import { error } from "console";
-import { unlink } from "fs/promises";
+import { rename, unlink } from "fs/promises";
 import { z } from "zod";
 export const t = initTRPC.create();
 export const appRouter = t.router({
-  getUser: t.procedure
-    //   .input(
-    //     z.object({
-    //       name: z.string(),
-    //     })
-    //   )
-    .input(z.string())
-    .query((req) => {
-      return { id: req.input, name: "Bilbo" };
-    }),
   deleteAsset: t.procedure.input(z.string()).mutation(async (req) => {
     try {
       await unlink(req.input);
-    } catch (e) {
+    } catch {
       throw new Error("File deletion error");
     }
-    return req.input;
+    return true;
   }),
+
+  renameAsset: t.procedure
+    .input(
+      z.object({
+        oldPath: z.string(),
+        newPath: z.string(),
+      })
+    )
+    .mutation(async (req) => {
+      try {
+        await rename(req.input.oldPath, req.input.newPath);
+      } catch (e) {
+        throw new Error("File rename error");
+      }
+      return true;
+    }),
 });

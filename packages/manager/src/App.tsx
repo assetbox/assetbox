741c690561565a8c48fe18a733e50e87787fe23c
moonchop <minssu913@naver.com>
2023-04-30 19:15:56 +0900

    fix(manager): redirect @trpc/server

--- packages/manager/src/App.tsx
+++ packages/manager/src/App.tsx
@@ -1,15 +1,7 @@
-import type { AppRouter } from "@assetbox/trpc";
-import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
 import { useState } from "react";
-// import { client } from "./client";
 
-const client = createTRPCProxyClient<AppRouter>({
-  links: [
-    httpBatchLink({
-      url: "http://localhost:5173/trpc",
-    }),
-  ],
-});
+import { client } from "./client";
+
 export function App() {
   const [count, setCount] = useState(0);
   const [text, setText] = useState("");

bad1264aa8c4f487d12b1e68503ce86b0d725c79
gronxb <gron1gh1@gmail.com>
2023-05-03 21:58:37 +0900

    feat(manager): inline svg isomorphic-dompurify

--- packages/manager/src/App.tsx
+++ packages/manager/src/App.tsx
@@ -1,3 +1,5 @@
+import { InlineSVG } from "./components/InlineSVG";
+
 interface AppProps {
   data: { assetFiles: string[]; dupeFiles: string[] };
 }
@@ -5,6 +7,7 @@ interface AppProps {
 export function App({ data }: AppProps) {
   return (
     <div style={{ display: "flex" }}>
+      <InlineSVG svgHtml='<svg viewBox="0 0 100 100"><circle cx="50" cy="50" r="50"/></svg>' />
       {data.assetFiles.map((v, idx) => (
         <img key={idx} src={v} style={{ width: "300px", height: "300px" }} />
       ))}

7044221a7874bc47abf001abc9908f8017ae0d71
devnak <showri0108@naver.com>
2023-05-04 18:39:11 +0900

    refactor(tools): remove findAssetFIles

--- packages/tools/src/__test__/findAssetFiles.test.ts
+++ packages/tools/src/__test__/findAssetFiles.test.ts
@@ -3,7 +3,7 @@ import { expect, jest, test } from "@jest/globals";
 import memfs, { vol } from "memfs";
 import process from "process";
 
-import { findAssetFiles } from "../findAssetFiles";
+import { findFilePathsFromGlob } from "../findFilePathsFromGlob";
 import { readAssetBoxConfig } from "../readAssetBoxConfig";
 import { createMockRepository } from "./utils/mockRepository";
 
@@ -18,13 +18,15 @@ describe("fileAssetFiles Test", () => {
 
     test("findAssetFiles Test", async () => {
       const config = await readAssetBoxConfig();
-      const result = await findAssetFiles(config.assetPaths, {
+      const result = await findFilePathsFromGlob(config.assetPaths, {
         fs: memfs,
       });
 
       expect(result).toStrictEqual([
+        "/test/normal/public/mock.md",
         "/test/normal/public/b.png",
         "/test/normal/public/a.png",
+        "/test/normal/src/assets/mock.md",
         "/test/normal/src/assets/d.png",
         "/test/normal/src/assets/c.png",
       ]);
@@ -50,7 +52,7 @@ describe("fileAssetFiles Test", () => {
 
     test("findAssetFiles Test", async () => {
       const config = await readAssetBoxConfig();
-      const result = await findAssetFiles(config.assetPaths, {
+      const result = await findFilePathsFromGlob(config.assetPaths, {
         fs: memfs,
       });
 
--- packages/tools/src/findFilePathsFromGlob.ts
+++ packages/tools/src/findFilePathsFromGlob.ts
@@ -1,10 +1,18 @@
-import glob from "glob";
+import glob, { GlobOptionsWithFileTypesUnset } from "glob";
 import { resolve } from "path";
 import { findPackageRoot } from "workspace-tools";
 
-export const findFilePathsFromGlob = async (globPatterns: string[]) => {
+export const findFilePathsFromGlob = async (
+  globPatterns: string[],
+  options?: GlobOptionsWithFileTypesUnset
+) => {
   const filePaths = await Promise.all(
-    globPatterns.map((pattern: string) => glob(pattern))
+    globPatterns.map((pattern: string) => {
+      return glob(pattern, {
+        cwd: findPackageRoot(process.cwd())!,
+        ...options,
+      });
+    })
   );
 
   return filePaths

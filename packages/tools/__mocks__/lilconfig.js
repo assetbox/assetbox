d647ddfa427db019c65230d912adf2c28705406a
hoonveloper <jh6car@gmail.com>
2023-05-04 16:32:47 +0900

    refactor(tools): rename variable

--- packages/tools/__mocks__/lilconfig.js
+++ packages/tools/__mocks__/lilconfig.js
@@ -7,19 +7,22 @@ const lilconfig = jest.createMockFromModule("lilconfig");
 lilconfig.lilconfig = function () {
   return {
     search: async () => {
-      if (vol.existsSync(path.resolve(process.cwd(), "assetbox.config.json"))) {
-        const data = JSON.parse(
-          vol.readFileSync(
-            path.resolve(process.cwd(), "assetbox.config.json"),
-            "utf-8"
-          )
-        );
+      if (
+        !vol.existsSync(path.resolve(process.cwd(), "assetbox.config.json"))
+      ) {
+        return null;
+      }
 
-        return {
-          filepath: path.resolve(process.cwd(), "assetbox.config.json"),
-          config: { assetPaths: data.assetPaths },
-        };
-      } else return null;
+      const config = JSON.parse(
+        vol.readFileSync(
+          path.resolve(process.cwd(), "assetbox.config.json"),
+          "utf-8"
+        )
+      );
+      return {
+        filepath: path.resolve(process.cwd(), "assetbox.config.json"),
+        config,
+      };
     },
   };
 };

fde181add754f02217e370cbdd127f6fc68f7e74
gronxb <gron1gh1@gmail.com>
2023-05-07 17:22:44 +0900

    feat(cli): ssr build after replace src assets

--- packages/cli/src/commands/staticBuild.ts
+++ packages/cli/src/commands/staticBuild.ts
@@ -1,11 +1,21 @@
+import { findFilePathsFromGlob, readAssetBoxConfig } from "@assetbox/tools";
 import react from "@vitejs/plugin-react-swc";
 import { readFile, writeFile } from "fs/promises";
+import { join, relative } from "path";
+import copy from "rollup-plugin-copy";
 import { renderStaticHtml } from "src/context/renderStaticHtml";
 import { build } from "vite";
+import { findPackageRoot } from "workspace-tools";
 
 import { resolveCliRoot, resolveProjectRoot } from "../utils/path";
 
 export const staticBuild = async () => {
+  const { assetPaths } = await readAssetBoxConfig();
+  const filePaths = await findFilePathsFromGlob(assetPaths);
+  const normalizeFilePaths = filePaths.map((filePath) =>
+    relative(findPackageRoot(process.cwd())!, filePath)
+  );
+
   let template = await readFile(
     resolveCliRoot("ssr", "templates", "index.html"),
     "utf-8"
@@ -18,7 +28,13 @@ export const staticBuild = async () => {
 
   await build({
     root: resolveCliRoot("ssr", "templates"),
-    plugins: [react()],
+    plugins: [
+      react(),
+      copy({
+        targets: [{ src: assetPaths, dest: join("assetbox-dist", "assets") }],
+        onlyFiles: true,
+      }),
+    ],
     publicDir: false,
     build: {
       outDir: resolveProjectRoot("assetbox-dist"),
@@ -30,5 +46,18 @@ export const staticBuild = async () => {
     "utf-8"
   );
   const html = await renderStaticHtml(template, "/");
-  await writeFile(resolveProjectRoot("assetbox-dist", "index.html"), html);
+  const staticHtml = normalizeFilePaths.reduce((originHtml, filePath) => {
+    const filename = filePath.split("/").pop();
+    if (!filename) {
+      return originHtml;
+    }
+
+    const filePathReg = new RegExp(filePath, "g");
+    return originHtml.replace(filePathReg, join("assets", filename));
+  }, html);
+
+  await writeFile(
+    resolveProjectRoot("assetbox-dist", "index.html"),
+    staticHtml
+  );
 };

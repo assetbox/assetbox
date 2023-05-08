e4288c319fb62ede7083e294b140f64c01e26083
Moonchop <82392767+moonchop@users.noreply.github.com>
2023-05-08 20:20:45 +0900

    init tailwind (#18)
    * feat(manager): init tailwind
    
    * feat(manager): set font, colors
    
    * feat(manager): add font-family
    
    * chore(manager): line break
    
    * feat(manager): add light blue

---
+++ packages/manager/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    colors: {
+      blue: {
+        DEFAULT: "#007AFF",
+        light: "#ECF5FF",
+      },
+      white: "#FFFFFF",
+      black: "#111111",
+      gray: {
+        DEFAULT: "#838A9A",
+        light: "#F3F6FA",
+        dark: "#454F5D",
+      },
+    },
+    extend: {
+      boxShadow: {
+        DEFAULT: "0px 4px 12px rgba(150, 154, 165, 0.16)",
+      },
+    },
+  },
+  plugins: [],
+};

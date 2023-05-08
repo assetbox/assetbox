1f98252cf763581aefb0b2f87d5ba6c0a7b46aed
moonchop <minssu913@naver.com>
2023-05-08 22:00:54 +0900

    chore(manager): delete css field

--- packages/manager/vite.config.ts
+++ packages/manager/vite.config.ts
@@ -1,11 +1,6 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
-import postcss from "./postcss.config.js";
-
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss,
-  },
 });

adae7dc46d421c66646df7085a5d5ea9dd2202a4
moonchop <minssu913@naver.com>
2023-05-10 22:52:07 +0900

    feat(manager): setting svg

--- packages/manager/vite.config.ts
+++ packages/manager/vite.config.ts
@@ -1,6 +1,7 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
 });

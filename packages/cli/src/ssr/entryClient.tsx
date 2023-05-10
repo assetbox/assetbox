f920502a56f6cc61a9dac28e52441be5907ab9bb
gronxb <gron1gh1@gmail.com>
2023-05-11 03:58:46 +0900

    fix(cli): import css

--- packages/cli/src/ssr/entryClient.tsx
+++ packages/cli/src/ssr/entryClient.tsx
@@ -1,7 +1,8 @@
+import "@assetbox/manager/dist/index.css";
+
 import { App } from "@assetbox/manager";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
 ReactDOM.hydrateRoot(
   document.getElementById("root") as Element,
   <BrowserRouter>

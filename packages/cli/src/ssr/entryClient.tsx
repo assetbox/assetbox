2a8f25a2905f52c2ecbf885190de940940778e73
gronxb <gron1gh1@gmail.com>
2023-05-11 04:05:28 +0900

    refactor: new line

--- packages/cli/src/ssr/entryClient.tsx
+++ packages/cli/src/ssr/entryClient.tsx
@@ -3,6 +3,7 @@ import "@assetbox/manager/dist/index.css";
 import { App } from "@assetbox/manager";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+
 ReactDOM.hydrateRoot(
   document.getElementById("root") as Element,
   <BrowserRouter>

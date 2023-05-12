ce747db1bc7d89525eaf40d2359a569aee320527
김지훈 <77804950+Hoonveloper@users.noreply.github.com>
2023-05-12 23:33:31 +0900

    mergeDupeFileSet (#23)
    * test(tools): make mergeDupeFileSet test
    
    * feat(tools): mergeDupeFileSet
    
    * fix(tools,trpc): update mergeDupeFileSet and trpc API
    
    * refactor(tools): re-position test files
    
    * refactor(tools): delete unnecessary code
    
    * refactor(tools): reflect review

---
+++ packages/tools/src/mergeDupeFileSet.test.ts
@@ -0,0 +1,60 @@
+jest.mock("fs");
+jest.mock("fs/promises");
+
+import memfs, { vol } from "memfs";
+import process from "process";
+
+import { findDupeFileSet } from "./findDupeFileSet";
+import { findFilePathsFromGlob } from "./findFilePathsFromGlob";
+import { mergeDupeFileSet } from "./mergeDupeFileSet";
+import { readAssetBoxConfig } from "./readAssetBoxConfig";
+import { createMockRepository } from "./testUtils/mockRepository";
+const spy = jest.spyOn(process, "cwd");
+
+describe("mergeDupeFileSet Test", () => {
+  beforeAll(() => {
+    spy.mockReturnValue("/test/dupe");
+    createMockRepository("/test/dupe", {
+      assetBoxConfig: true,
+      isDupe: true,
+    });
+  });
+
+  test("Dupe Repository", async () => {
+    const config = await readAssetBoxConfig();
+
+    const assetFiles = await findFilePathsFromGlob(config.assetPaths, {
+      fs: memfs,
+    });
+
+    const dupeFileSetFromAssetFiles = await findDupeFileSet(assetFiles);
+
+    const mergedDupFileSet = await mergeDupeFileSet(
+      dupeFileSetFromAssetFiles[0],
+      "/test/dupe/public/merged.png"
+    );
+
+    expect(mergedDupFileSet).toEqual({
+      isMerged: true,
+    });
+
+    expect(vol.readdirSync("/test/dupe/public")).toStrictEqual([
+      "a.png",
+      "a_copy.png",
+      "merged.png",
+      "mock.md",
+    ]);
+
+    expect(vol.readdirSync("/test/dupe/src/assets")).toStrictEqual([
+      "a.png",
+      "c.png",
+      "d.png",
+      "mock.md",
+    ]);
+  });
+
+  afterAll(() => {
+    spy.mockClear();
+    vol.rmdirSync("/test/dupe/", { recursive: true });
+  });
+});

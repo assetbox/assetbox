fd711b65391f3a21d9eccdd89fab88a43935b8a0
moonchop <minssu913@naver.com>
2023-05-16 14:01:01 +0900

    feat(manager): add rotate Icon

--- packages/manager/src/components/ui/Select.tsx
+++ packages/manager/src/components/ui/Select.tsx
@@ -1,5 +1,5 @@
-import { Listbox } from "@headlessui/react";
-import { ComponentProps } from "react";
+import { Listbox, Transition } from "@headlessui/react";
+import { ComponentProps, ReactNode } from "react";
 
 import DownVector from "../../assets/down-vector.svg";
 import { cn } from "../../utils/cn";
@@ -27,19 +27,34 @@ Select.Button = ({
   return (
     <Listbox.Button
       className={cn(
-        "w-[165px] h-12 flex items-center  rounded bg-white px-[10px] mb-1"
+        "w-[165px] h-12 flex items-center rounded bg-white px-[10px] mb-1"
       )}
       {...props}
     >
-      <>
-        {children}
-        <DownVector className="ml-auto" />
-      </>
+      {({ open }) => (
+        <>
+          {children}
+          <DownVector
+            className={cn("ml-auto", open && "rotate-180 transform")}
+          />
+        </>
+      )}
     </Listbox.Button>
   );
 };
 Select.Options = ({ ...props }: ComponentProps<typeof Listbox.Options>) => {
-  return <Listbox.Options {...props} />;
+  return (
+    <Transition
+      enter="transition duration-100 ease-out"
+      enterFrom="transform scale-95 opacity-0"
+      enterTo="transform scale-100 opacity-100"
+      leave="transition duration-75 ease-out"
+      leaveFrom="transform scale-100 opacity-100"
+      leaveTo="transform scale-95 opacity-0"
+    >
+      <Listbox.Options {...props} />
+    </Transition>
+  );
 };
 
 type ClassNameArgs = {
@@ -51,7 +66,7 @@ Select.Option = ({
   className,
   ...props
 }: ComponentProps<typeof Listbox.Option> & {
-  className?: (args: ClassNameArgs) => string | string;
+  className?: ((args: ClassNameArgs) => string) | string;
 }) => {
   return (
     <Listbox.Option
@@ -59,7 +74,7 @@ Select.Option = ({
       className={({ active, selected }: ClassNameArgs) =>
         cn(
           "bg-white cursor-pointer text-base w-[165px] h-12 flex items-center border-b border-gray-light last:border-b-0 px-[10px]",
-          "first:rounded last:rounded",
+          "first:rounded-t last:rounded-b",
           active && "text-blue",
           typeof className === "function"
             ? className({ active, selected })

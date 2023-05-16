09dd7a0e3da2fcedaf59a596394a995c8b5e0090
moonchop <minssu913@naver.com>
2023-05-16 17:38:23 +0900

    refactor(manager): apply review, fix types

---
+++ packages/manager/src/components/ui/Listbox.tsx
@@ -0,0 +1,74 @@
+import {
+  Listbox,
+  ListboxButtonProps,
+  ListboxOptionProps,
+  ListboxOptionsProps,
+  Transition,
+} from "@headlessui/react";
+import { ComponentProps } from "react";
+
+import DownVector from "../../assets/down-vector.svg";
+import { cn } from "../../utils/cn";
+
+export const ListBox = ({ ...props }: ComponentProps<typeof Listbox>) => {
+  return <Listbox {...props} />;
+};
+
+ListBox.Button = ({
+  children,
+  className,
+  ...props
+}: ListboxButtonProps<"button">) => {
+  return (
+    <Listbox.Button
+      className={(bag) =>
+        cn(
+          "w-[165px] h-12 flex items-center rounded bg-white px-[10px] mb-1",
+          typeof className === "function" ? className(bag) : className
+        )
+      }
+      {...props}
+    >
+      {({ open }) => (
+        <>
+          {children}
+          <DownVector
+            className={cn("ml-auto", open && "rotate-180 transform")}
+          />
+        </>
+      )}
+    </Listbox.Button>
+  );
+};
+
+ListBox.Options = ({ ...props }: ListboxOptionsProps<"ul">) => {
+  return (
+    <Transition
+      className={"inline-block"}
+      enter="transition duration-100 ease-in"
+      enterFrom="transform scale-95 opacity-0"
+      enterTo="transform scale-100 opacity-100"
+      leave="transition duration-75 ease-in"
+      leaveFrom="transform scale-100 opacity-100"
+      leaveTo="transform scale-95 opacity-0"
+    >
+      <Listbox.Options {...props} />
+    </Transition>
+  );
+};
+
+ListBox.Option = ({ className, ...props }: ListboxOptionProps<"li", any>) => {
+  return (
+    <Listbox.Option
+      {...props}
+      className={(bag) =>
+        cn(
+          "bg-white cursor-pointer text-base w-[165px] h-12 flex items-center border-b border-gray-light last:border-b-0 px-[10px]",
+          "first:rounded-t last:rounded-b",
+          bag.active && "text-blue",
+          typeof className === "function" ? className(bag) : className
+        )
+      }
+    />
+  );
+};
--- packages/manager/src/components/ui/index.ts
+++ packages/manager/src/components/ui/index.ts
@@ -1,3 +1,4 @@
 export * from "./CheckBox";
 export * from "./InlineSVG";
+export * from "./ListBox";
 export * from "./ProgressBar";

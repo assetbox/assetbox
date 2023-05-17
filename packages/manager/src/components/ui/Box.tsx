e4e55eb71c1a72874f40de99ac03f1395d709146
Sungyu Kang <gron1gh1@gmail.com>
2023-05-17 21:07:03 +0900

    refactor(manager): box

--- packages/manager/src/components/ui/Box.tsx
+++ packages/manager/src/components/ui/Box.tsx
@@ -2,12 +2,9 @@ import { cn } from "../../utils";
 
 export const Box = ({
   className,
-  children,
   ...props
-}: React.PropsWithChildren<React.HTMLAttributes<HTMLDivElement>>) => {
+}: React.HTMLAttributes<HTMLDivElement>) => {
   return (
-    <div className={cn("bg-white shadow rounded-lg", className)} {...props}>
-      {children}
-    </div>
+    <div className={cn("bg-white shadow rounded-lg", className)} {...props} />
   );
 };

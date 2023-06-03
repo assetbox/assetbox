d4a71a142792fe0e4785e275cb623fb33e19c33c
hoonveloper <jh6car@gmail.com>
2023-06-03 23:41:43 +0900

    feat: getAssetHash(md5)

--- packages/cli/src/commands/uploadFileRouter.ts
+++ packages/cli/src/commands/uploadFileRouter.ts
@@ -1,12 +1,33 @@
 import { type RequestHandler, Router } from "express";
+import md5File from "md5-file";
 import multer from "multer";
-const uploadFileRouter = Router();
-const upload = multer({ dest: __dirname + "/public/uploads/" });
+export const uploadFileRouter = Router();
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, __dirname); //
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname); //
+  },
+});
+
+const upload = multer({ storage: storage });
 
 const uploadFile: RequestHandler = async (req, res) => {
   try {
-    const files = req;
+    const files: any = req.files;
     console.log(files);
+    /* 
+    md5 기반 해쉬 구하는 로직
+    */
+    md5File(files[0].path).then((hash) => {
+      console.log(`The hash of files[0].path is: ${hash}`);
+    });
+    /*
+    중복 validation check 로직 구현
+    */
+
+    res.status(406).json({ message: "Asset uploaded successfully" });
   } catch (e) {
     throw new Error("Asset upload Error" + e);
   }
@@ -20,4 +41,3 @@ const test: RequestHandler = async (req, res) => {
 };
 uploadFileRouter.post("/", upload.array("assets"), uploadFile);
 uploadFileRouter.get("/", test);
-module.exports = uploadFileRouter;

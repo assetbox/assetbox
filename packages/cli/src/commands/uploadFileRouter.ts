8fc872582d03cea36e86544437e88aff163feb81
hoonveloper <jh6car@gmail.com>
2023-06-06 20:59:03 +0900

    fix: remove / using sep

--- packages/cli/src/commands/uploadFileRouter.ts
+++ packages/cli/src/commands/uploadFileRouter.ts
@@ -7,7 +7,7 @@ import {
 import { type RequestHandler, Router } from "express";
 import fs from "fs";
 import multer from "multer";
-import path, { sep } from "path";
+import path, { join, sep } from "path";
 
 export const uploadFileRouter = Router();
 
@@ -15,10 +15,9 @@ const uploadAsset = (savePath?: string) =>
   multer({
     storage: multer.diskStorage({
       destination: function (req, file, cb) {
-        const resolvePath =
-          savePath === undefined
-            ? `${cwd()}/${req.body.savePath}`
-            : `${cwd()}/${savePath}`;
+        const resolvePath = savePath
+          ? join(cwd(), sep, savePath)
+          : join(cwd(), sep, req.body.savePath);
 
         if (!fs.existsSync(`${resolvePath}`)) {
           fs.mkdirSync(`${resolvePath}`);
@@ -36,7 +35,6 @@ const uploadFile: RequestHandler = async (req, res) => {
     const checkingPath = [cwd(), req.body.savePath, req.file?.originalname]
       .filter(Boolean)
       .join(path.sep);
-    console.log(checkingPath);
 
     if (!fs.existsSync(checkingPath)) {
       res.status(400).json({ message: "Asset upload Failed" });
@@ -58,10 +56,12 @@ interface ConvertedResult {
   dupePaths: string[];
 }
 function getExtension(name: string) {
-  const extension = name.slice(name.lastIndexOf("."));
+  const extension = name.split(".").pop()!;
 
-  if (extension === ".svg") return "svg+xml";
-  return extension.slice(1);
+  if (["svg"].includes(extension)) {
+    return "svg+xml";
+  }
+  return extension;
 }
 function convertImageObject(imageObject: ImageObject): ConvertedResult[] {
   const result: ConvertedResult[] = [];
@@ -69,15 +69,18 @@ function convertImageObject(imageObject: ImageObject): ConvertedResult[] {
   for (const key in imageObject) {
     // eslint-disable-next-line no-prototype-builtins
     if (imageObject.hasOwnProperty(key)) {
-      const fileNameWithExtension = key.substring(key.lastIndexOf("/") + 1);
+      const fileNameWithExtension = key.substring(key.lastIndexOf(sep) + 1);
 
       const extension = getExtension(
-        `${cwd()}/.assetbox/${fileNameWithExtension}`
+        path.join(cwd(), sep, ".assetbox", sep, fileNameWithExtension)
       );
 
       const savePath = key;
-      const base64Image = `data:image/${extension};base64, ${fs.readFileSync(
-        `${cwd()}/.assetbox/${fileNameWithExtension}`,
+      console.log(extension);
+      const prefix = ["data:image", `${extension};base64`].join(sep);
+
+      const base64Image = `${prefix},${fs.readFileSync(
+        path.join(cwd(), sep, ".assetbox", sep, fileNameWithExtension),
         {
           encoding: "base64",
         }
@@ -99,21 +102,23 @@ const getValidationInfo: RequestHandler = async (req, res) => {
   const { categories } = await readAssetBoxConfig();
   const assetFiles = Object.values(categories).flat(); // 모든파일
 
-  const getAddedFiles = fs.readdirSync(`${cwd()}/.assetbox/`); // 추가된 파일 watingValidation 폴더에서 가져오기
+  const getAddedFiles = fs.readdirSync(path.join(cwd(), sep, ".assetbox", sep)); // 추가된 파일 watingValidation 폴더에서 가져오기
   console.log(getAddedFiles);
   const fileList: { path: string; hash: string }[] = [];
 
   getAddedFiles.map(async (file) => {
     const json = { path: "", hash: "" };
-    json.path = req.body.savePath + "/" + file;
-    json.hash = await createFileHash(cwd() + "/.assetbox/" + file);
+    json.path = path.join(req.body.savePath, sep, file);
+    json.hash = await createFileHash(
+      path.join(cwd(), sep, ".assetbox", sep, file)
+    );
     fileList.push(json);
   });
   console;
 
   const result = await getDupeFiles(assetFiles, fileList);
   const convertedResult: ConvertedResult[] = convertImageObject(result);
-  fs.rmdirSync(cwd() + "/.assetbox", { recursive: true });
+  fs.rmdirSync(path.join(cwd(), sep, ".assetbox"), { recursive: true });
   res.status(201).json([...convertedResult]);
 };
 const test: RequestHandler = async (req, res) => {
